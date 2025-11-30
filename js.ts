fce590e5a7766e054c989100a6f309302e90c3fa
pooyan <thepooyan@gmail.com>
2025-11-30 13:52:32 +0330

    nothing!

--- js.ts
+++ js.ts
@@ -1,4 +1,15 @@
 const newLogin = $("#newLogin")
 if (newLogin) {
-    const contentWindow = newLogin.find("#mainContent")
+
+    $('[data-target]').on("click", (e) => {
+        // activate self
+        $('[data-target]').removeClass("active")
+        $(e.currentTarget).addClass("active")
+        //activate target
+        const target = $(e.currentTarget).attr("data-target") || "";
+        const group = $(target).attr("data-group") || "";
+        console.log(group)
+        $(`[data-group="${group}"]`).removeClass("active");
+        $(target).addClass("active");
+    })
 }
